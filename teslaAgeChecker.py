2a1153e5e098e5f0d2d81f075e9c8da06029bb52
tayu1994@gmail.com <TayaZaya1994>
2022-04-08 18:33:48 +0300

    init teslaAgeChecker.py

--- teslaAgeChecker.py
+++ teslaAgeChecker.py
@@ -1,5 +1,3 @@
-
-
 """
 age = input("What is your age?: ")
 
@@ -19,14 +17,61 @@ elif int(age) == 18:
 #also make it so that the default age is set to 0 if no argument is given.
 """
 
+"""
+def checkDriverAge(age = 0):
+	if int(age) < 18:
+		print("Sorry, you are too young to drive this car. Powering off")
+	elif int(age) > 18:
+		print("Powering On. Enjoy the ride!");
+	elif int(age) == 18:
+		print("Congratulations on your first year of driving. Enjoy the ride!")
+# Tests
+# Test 1 default age 0
+checkDriverAge()
 
+# test 1 young age
+checkDriverAge(1)
+checkDriverAge(2)
+checkDriverAge(-7)
+checkDriverAge(14)
+#checkDriverAge(a)
+checkDriverAge(3)
+checkDriverAge(9)
 
+# Test case 2
+# testing legal age
+checkDriverAge(17.9999999999999999)
+checkDriverAge(18)
+checkDriverAge(17.787)
 
-age = input("What is your age?: ")
+# Test case 3
+# testing above legal age
+checkDriverAge(28)
+checkDriverAge(90)
+checkDriverAge(29)
+checkDriverAge(57.8)
 
-if int(age) < 18:
-	print("Sorry, you are too young to drive this car. Powering off")
-elif int(age) > 18:
-	print("Powering On. Enjoy the ride!");
-elif int(age) == 18:
-	print("Congratulations on your first year of driving. Enjoy the ride!")
+"""
+
+"""
+def checkDriverAge():
+    age = input("What is your age?: ")
+    if int(age) < 18:
+        print("Sorry, you are too young to drive this car. Powering off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!");
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
+checkDriverAge()
+"""
+"""
+def checkDriverAge(age=0):
+    if int(age) < 18:
+        print("Sorry, you are too yound to drive this car. Powering off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!");
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
+checkDriverAge(18)
+"""
+help(help)
